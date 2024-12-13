2255bc4367264b823af6e4cdcbad9036c16964a4
Youssef Souilah <souilah.youssef.0@gmail.com>
2024-12-13 15:05:08 +0100

    EX2

---
+++ TP_TD2/src/EX2/Main.java
@@ -0,0 +1,38 @@
+package EX2;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		/*
+		 * EX1
+		 * Scanner sc=new Scanner(System.in);
+			System.out.print("Entrer le mot :");
+			char[] mot =sc.next().toCharArray();
+			
+			for (int i =mot.length-1;i>=0;i--) {
+				System.out.print(mot[i]);
+			}
+		 * 
+		 * EX2
+		 * non
+		 * 
+		 * EX3
+		 * isEmpty()
+		 * 
+		 * EX4
+		 * toUpperCase()
+		 * 
+		 * EX5
+		 * toLowerCase()
+		 * 
+		 */
+		
+		
+		
+	}
+
+}
