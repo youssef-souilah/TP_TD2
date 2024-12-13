d2a2e493e456bb9445f09aec084ae20441815533
Youssef Souilah <souilah.youssef.0@gmail.com>
2024-12-13 15:05:00 +0100

    EX1

---
+++ TP_TD2/src/EX1/Main.java
@@ -0,0 +1,76 @@
+package EX1;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner sc=new Scanner(System.in);
+		
+		/*
+		 * Ex1
+		 * 
+		 * System.out.print("Entrer un nombre entier; ");
+		int nb=sc.nextInt();
+		
+		System.out.println();
+		
+		System.out.print("Entrer un mot : ");
+		String str=sc.next();
+		
+		System.out.println(nb+" "+str); */
+		
+		
+		/*
+		 * Ex2
+		System.out.print("Entrer un mot; ");
+		String mot1=sc.next();
+		
+		sc.nextLine();
+		
+		System.out.print("Entrer un mot : ");
+		String mot2=sc.next();
+		
+		if(mot1.equals(mot2)) {
+			System.out.println("equal");
+		}
+		else {
+			System.out.println("not equal");
+		}
+		
+		*/
+		/*
+		 * EX4
+		 
+		System.out.print("Entrer un mot; ");
+		String mot1=sc.next().toLowerCase();
+		
+		sc.nextLine();
+		
+		System.out.print("Entrer un mot : ");
+		String mot2=sc.next().toLowerCase();
+		
+		if(mot1.equals(mot2)) {
+			System.out.println("equal");
+		}
+		else {
+			System.out.println("not equal");
+		}
+		*/
+		/*
+		 * Ex5
+		 * 
+		 * java
+		 * javacode
+		 * java code
+		 */
+		
+		/*
+		 * EX6
+		 * toString()
+		 */
+	}
+
+}
